0b94d9b1fc7493bf76a3c149f99b40d937f8e139
ugo070 <ugo070@yahoo.com>
2020-01-13 22:06:43 -0500

    regularized

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  Article: require("./articles"),
-  Comment: require("./comments")
+  Headline: require("./Headline"),
+  Note: require("./Note")
 };
--- server.js
+++ server.js
@@ -1,159 +1,43 @@
+// Web Scraper Homework Solution Example
+// (be sure to watch the video to see
+// how to operate the site in the browser)
+// -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+
+// Require our dependencies
 var express = require("express");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
-ObjectId = require('mongodb').ObjectID;
-
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
-
+// Set up our port to be either the host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
 
+// Instantiate our Express App
 var app = express();
 
+// Require our routes
+var routes = require("./routes");
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Make public a static folder
 app.use(express.static("public"));
 
+// Connect Handlebars to our Express app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Have every request go through our route middleware
+app.use(routes);
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
-app.get("/scrape", function (req, res) {
-    axios.get("https://news.google.com/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen").then(function (response) {
-        var $ = cheerio.load(response.data);
-        //console.log(response.data);
-
-        $("article h3").each(function (i, element) {
-            var result = {};
-
-            result.headline = $(this)
-                .text();
-
-            result.summary = $(this)
-                .next()
-                .children("span")
-                .text();
-
-            result.url = "https://news.google.com" + $(this)
-                .children("a")
-                .attr("href");
-
-            result.photo_url = $(this)
-                .parents()
-                .parent()
-                .children("a")
-                .children("figure")
-                .children("img")
-                .attr("src");
-
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    console.log(err);
-                })
-        });
-
-        res.send("Scrape Complete");
-        res.redirect("/");
-
-    })
-});
-
-app.get("/articles", function (req, res) {
-    db.Article.find({})
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
-
-app.get("/comments/:articleID", function (req, res) {
-    db.Comment.find({ article_id: req.params.articleID })
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-            res.send(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-})
-
-app.delete("/clear-articles", function (req, res) {
-    db.Article.remove({})
-        .then(function (dbArticle) {
-            console.log(dbArticle);
-        })
-    db.Comment.remove({})
-        .then(function (dbArticle) {
-            console.log(dbArticle);
-        })
-    console.log("Article API called!");
-});
-
-app.post("/save-article/:id", function (req, res) {
-    var savedID = req.params.id;
-    console.log(savedID);
-    db.Article.update({ _id: ObjectId(savedID) }, { $set: { "saved": true } }, function (err, result) {
-        var response;
-        if (err) {
-            response = {'status':200,'message':'An error has occurred'}
-        } else {
-            response = {'status':404,'message':'' + result + ' document(s) updated'}
-        }
-        res.send(response);
-    }).then(function (dbArticle) {
-        console.log(dbArticle);
-    })
-});
-
-app.post("/remove-article/:id", function (req, res) {
-    var savedID = req.params.id;
-    console.log(savedID);
-    db.Article.update({ _id: ObjectId(savedID) }, { $set: { "saved": false } }, function (err, result) {
-        if (err) {
-            console.log('Error updating object: ' + err);
-            res.send({ 'error': 'An error has occurred' });
-        } else {
-            console.log('' + result + ' document(s) updated');
-            res.send(result);
-        }
-    })
-});
-
-app.post("/add-comment/:note/:articleID", function (req, res) {
-    var comment = {};
-    var userComment = req.params.note;
-    var articleID = req.params.articleID;
-    console.log(userComment);
-    console.log(articleID);
-
-    comment = {
-        comment: userComment,
-        article_id: articleID
-    }
-
-    db.Comment.create(comment)
-        .then(function (dbArticle) {
-            console.log(dbArticle);
-            res.send(dbArticle);
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-});
-
-
-
-
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
 });
